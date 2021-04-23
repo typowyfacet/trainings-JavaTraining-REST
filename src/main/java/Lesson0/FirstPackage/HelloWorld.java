0dbdafb15588c6a1bbaa7edc2f9f3c768eba815f
rhaniewski <rhaniewski@sii.pl>
2021-04-23 14:09:00 +0200

    initial commit

---
+++ src/main/java/Lesson0/FirstPackage/HelloWorld.java
@@ -0,0 +1,8 @@
+package Lesson0.FirstPackage;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ src/main/java/Lesson1/TypesAndVariables/Primitives.java
@@ -0,0 +1,23 @@
+package Lesson1.TypesAndVariables;
+
+public class Primitives {
+
+
+    public static void main(String[] args) {
+
+        int someNumber = 1;
+        int otherNumber = -10;
+        System.out.println("this is an number " + someNumber + " ant this is other " + otherNumber);
+
+        boolean logicalPositiveValue = true;
+        boolean logicalNegativeValue = false;
+        System.out.println("logical values " + logicalNegativeValue + " / " + logicalPositiveValue);
+
+        double floatingPointNumber = 12.21;
+        float floatNumber = 21.12f;
+        System.out.println("floating points values " + floatingPointNumber + " / " + floatNumber);
+
+
+        //TODO declare 2 variables each type and print it.
+    }
+}
