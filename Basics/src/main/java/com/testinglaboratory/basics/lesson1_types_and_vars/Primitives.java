830e7a5140c6d6d58e9afb04a8a893e2c58c3503
Przemysław Kasprzak <przemyslaw.kasprzak@pl.abb.com>
2022-11-05 10:23:24 +0100

    declared somple Primitives

--- Basics/src/main/java/com/testinglaboratory/basics/lesson1_types_and_vars/Primitives.java
+++ Basics/src/main/java/com/testinglaboratory/basics/lesson1_types_and_vars/Primitives.java
@@ -3,21 +3,27 @@ package com.testinglaboratory.basics.lesson1_types_and_vars;
 public class Primitives {
 
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        int someNumber = 1;
-        int otherNumber = -10;
-        System.out.println("this is an number " + someNumber + " ant this is other " + otherNumber);
+    int someNumber = 1;
+    int otherNumber = -10;
+    System.out.println("this is an number " + someNumber + " ant this is other " + otherNumber);
 
-        boolean logicalPositiveValue = true;
-        boolean logicalNegativeValue = false;
-        System.out.println("logical values " + logicalNegativeValue + " / " + logicalPositiveValue);
+    boolean logicalPositiveValue = true;
+    boolean logicalNegativeValue = false;
+    System.out.println("logical values " + logicalNegativeValue + " / " + logicalPositiveValue);
 
-        double floatingPointNumber = 12.21;
-        float floatNumber = 21.12f;
-        System.out.println("floating points values " + floatingPointNumber + " / " + floatNumber);
+    double floatingPointNumber = 12.21;
+    float floatNumber = 21.12f;
+    System.out.println("floating points values " + floatingPointNumber + " / " + floatNumber);
 
 
-        //TODO declare 2 variables each type and print it.
-    }
+    //TODO declare 2 variables each type and print it.
+
+    float someNewFloat = 10.44f;
+    double someNewDouble = 10.5d;
+
+    System.out.println(someNewFloat);
+    System.out.println(someNewDouble);
+  }
 }
